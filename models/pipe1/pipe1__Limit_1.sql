f413b3ef712e26005ff7cd250a9dbe9f368f030c
saraf.anubhav@prophecy.io <saraf.anubhav@prophecy.io>
2026-07-13 14:24:50 +0000

    Replace `Reformat_1` with `Limit_1` transformation in `pipe1` pipeline

--- models/pipe1/pipe1__Limit_1.sql
+++ models/pipe1/pipe1__Limit_1.sql
@@ -11,9 +11,7 @@ WITH static_id_and_name AS (
   {#Provides a fixed default record (id=1, name='abc') to serve as a stable reference or fallback label in reports and joins.#}
   SELECT 
     1 AS id,
-    "abc" AS name,
-    "addr_1" AS address,
-    23 AS val
+    "abc" AS name
 
 ),
 
@@ -23,8 +21,26 @@ Reformat_1 AS (
   
   FROM static_id_and_name AS in0
 
+),
+
+Reformat_2 AS (
+
+  SELECT * 
+  
+  FROM Reformat_1 AS in0
+
+),
+
+Limit_1 AS (
+
+  SELECT * 
+  
+  FROM Reformat_2 AS in0
+  
+  LIMIT 10
+
 )
 
 SELECT *
 
-FROM Reformat_1
+FROM Limit_1
